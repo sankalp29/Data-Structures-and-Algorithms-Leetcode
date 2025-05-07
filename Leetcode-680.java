eb6d77a3498b55f29ef4575888d803559be3dc61
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-05-07 19:37:31 +0530

    Code for Leetcode-680 : Valid palindrome II

---
+++ Leetcode-680.java
@@ -0,0 +1,32 @@
+/*
+    https://leetcode.com/problems/valid-palindrome-ii/
+*/
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        
+        int n = s.length();        
+        int left = 0, right = n-1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return checkPalindrome(s, left+1, right) || checkPalindrome(s, left, right-1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean checkPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
