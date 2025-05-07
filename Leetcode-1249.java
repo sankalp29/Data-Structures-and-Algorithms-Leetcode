bdf98dd11a8715548af37e97fd3475202e06cd6d
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-05-07 14:02:43 +0530

    Solved using stack method as well

--- Leetcode-1249.java
+++ Leetcode-1249.java
@@ -29,14 +29,9 @@ class Solution {
             }
         }
 
-        // Second pass: Remove the unmatching opening brackets
-        return removeUnmatchedOpeningBrackets(sb, openingBrackets);
-    }
-
-    /**
-     * Removes the unmached opening brackets in the expression
-     */
-    private String removeUnmatchedOpeningBrackets(StringBuilder expression, int openingBrackets) {
+        /**
+        * Second pass: Removes the unmached opening brackets in the expression
+        */
         StringBuilder result = new StringBuilder();
         for (int i = expression.length() - 1; i >= 0; i--) {
             char ch = expression.charAt(i);
@@ -45,5 +40,35 @@ class Solution {
         }
 
         return result.reverse().toString();
+    }
+
+
+    /**
+     * Use the stack method to remove minimum brackets to make the expression valid.
+     * Time complexity : O(L) + O(L) => O(2L) => O(L)
+     * Space complexity : O(L)
+    */
+    private String useStackMethod(String s) {
+        Set<Integer> indexesToDelete = new HashSet<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') st.add(i);
+            else if (s.charAt(i) == ')') {
+                if (st.isEmpty()) indexesToDelete.add(i);
+                else st.pop();
+            }
+        }
+        
+        while (!st.isEmpty()) indexesToDelete.add(st.pop());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToDelete.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
     }
 }
