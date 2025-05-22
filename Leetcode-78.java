c44306bdfd23a5e5723b9e4a5232db3e2312105e
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-05-22 22:44:41 +0530

    Code for Leetcode-78 : Subsets

--- Leetcode-78.java
+++ Leetcode-78.java
@@ -1,12 +1,35 @@
 /*
-    
+    https://leetcode.com/problems/subsets/
 */
 
 class Solution {
     /**
+     * Bitmasking
      * Time complexity : O(N * 2^N)
      * Space complexity : O(N * 2^N)
      */
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> powerSet = new ArrayList<>();
+        int totalSubsets = 1 << n;
+
+        for (int mask = 0; mask < totalSubsets; mask++) { // mask is in a way the subset number
+            List<Integer> subset = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) subset.add(nums[i]);
+            }
+
+            powerSet.add(subset);
+        }
+
+        return powerSet;
+    }
+
+    /**
+     * Time complexity : O(N * 2^N)
+     * Space complexity : O(N * 2^N)
+     */
+    /*
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> powerSet = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
@@ -15,6 +38,7 @@ class Solution {
 
         return powerSet;
     }
+    */
 
     private void generatePowerSet(int[] nums, int index, List<Integer> current, List<List<Integer>> powerSet) {
         if (index == nums.length) {
