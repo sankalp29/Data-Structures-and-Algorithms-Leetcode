6b8fdbed1a226d71f383298c4b4178bc5632b805
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-08-21 01:06:36 +0530

    Code for Leetcode-875 : Koko Eating Bananas

---
+++ Leetcode-875.java
@@ -0,0 +1,35 @@
+/**
+ * Time complexity : O(NlogB) (B = max bananas in a pile)
+ * Space complexity : O(1)
+ */
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        long bananas = 0;
+        for (int pile : piles) bananas = Math.max(bananas, pile);
+
+        long lower = 1, upper = bananas, minEatPerHour = -1;
+
+        while (lower <= upper) {
+            long eatPerHour = lower + (upper - lower) / 2;
+            long hoursNeeded = calculateHoursNeeded(piles, eatPerHour);
+
+            if (hoursNeeded > h) lower = eatPerHour + 1;
+            else {
+                minEatPerHour = eatPerHour;
+                upper = eatPerHour - 1;
+            }
+        }
+
+        return (int) minEatPerHour;
+    }
+
+    private long calculateHoursNeeded(int[] piles, long eatPerHour) {
+        long hoursNeeded = 0;
+        for (int bananas : piles) {
+            if (bananas % eatPerHour == 0) hoursNeeded+=(bananas / eatPerHour);
+            else hoursNeeded+= (bananas + eatPerHour) / eatPerHour;
+        }
+
+        return hoursNeeded;
+    }
+}
