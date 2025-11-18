7a081476d2b0e845c3717ccbc2ad3f52b5138278
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-11-18 13:25:22 +0530

    Code for Leetcode-433 : Minimum Genetic Mutation

---
+++ Leetcode-433.java
@@ -0,0 +1,47 @@
+https://leetcode.com/problems/minimum-genetic-mutation/
+
+class Solution {
+    /**
+     * Time complexity : O(N * L)
+     * Space complexity : O(N * L)
+     */
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        if (startGene.equals(endGene)) return 0;
+
+        Set<String> geneBank = new HashSet<>();
+        for (String gene : bank) geneBank.add(gene);
+
+        Queue<String> bfsQueue = new ArrayDeque<>();
+        bfsQueue.add(startGene);
+        geneBank.remove(startGene);
+        int distance = 0;
+        char[] choices = {'A', 'C', 'G', 'T'};
+
+        while (!bfsQueue.isEmpty()) {
+            int size = bfsQueue.size();
+
+            while (size-- > 0) {
+                String gene = bfsQueue.poll();
+
+                if (gene.equals(endGene)) return distance;
+                char[] current = gene.toCharArray();
+                
+                for (int i = 0; i < gene.length(); i++) {
+                    for (char ch : choices) {
+                        current[i] = ch;
+                        String mutated = new String(current);
+                        if (geneBank.contains(mutated)) {
+                            bfsQueue.add(mutated);
+                            geneBank.remove(mutated);
+                        }
+                    }
+                    current[i] = gene.charAt(i);
+                }
+            }
+
+            distance++;
+        }
+
+        return -1;
+    }
+}
